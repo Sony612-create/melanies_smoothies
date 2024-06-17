262c4fcd516f03bd4ca3221afdc17bc94493069f
Sony612-create <sony.jelaris@dataentrega.com>
2024-06-17 16:18:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,8 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
 import pandas
-
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -31,64 +29,31 @@ ingredients_list = st.multiselect(
     , max_selections=5)
 
 if ingredients_list:
-   ingredients_string =''
+   ingredients_string = ''
+
+   for fruit_chosen in ingredients_list:
+           ingredients_string += fruit_chosen + ''
+       
+           search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+           st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+       
+           st.subheader(fruit_chosen + 'Nutrition Information')
+           fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+           fv_df = st.dataframe(data= fruityvice_response.json(),use_container_width=True)
+
+   #st.write(ingredients_string)
+
+   my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('""" + ingredients_string + """' ,'"""+name_on_order+"""')"""
+
+   #st.write(my_insert_stmt)
+   #st.stop()
+   time_to_insert = st.button('Submit Order')
+
+   if time_to_insert:
+        session.sql(my_insert_stmt).collect()
    
-   
-    Import python packages
-import streamlit as st
-import pandas as pd
-from snowflake.snowpark.context import get_active_session
-
-df_calculate_hash(df):
-   concatenated_values = df.astype(str).values.sum()
-   hash_value = hash(concatenated_values)
-   return hash_value
-   
-   
-# Write directly to the app
-st.title(":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
-st.write("""Orders that need to filled.""")
-
-#Get the active session
-session = get_active_session()
-my_dataframe = session.table("Smoothies.public.orders").to_pandas()
-
-if not my_dataframe empty:
-    editable_df =st.data_editor(my_dataframe)
-    submitted = st.button('Submit')
-	
-	if submitted:
-	    try:
-		  
-		    hash_before_update = calculate_hash(my_dataframe)
-			
-			for index, row in editable_df.iterrows():
-			    order_uid = row['ORDER_UID']
-				order_filled =row['ORDER_FILLED']
-				
-				if pd.isna(order_uid) or pd.isna(order_filled):
-				     continue
-					 
-				if pd.isna(order_uid) or pd.isna(order_filled):
-				     continue
-                order_filled = 'TRUE' if order_filled else 'FALSE'
-
-                session.sql(f"""
-                    UPDATE smoothies.public.orders
-                    SET order_filled = {order_filled}
-                    WHERE order_uid	= {order_uid}
-                """).collect()
+   st.success('Your Smoothie is ordered!', icon="✅")
 
 
-            updated_dataframe = session.table("smoothies.public.orders").to_pandas()
 
-            hash_after_update = calculate_hash(updated_dataframe)
-
-            if hash_before_update == hash_after_update:
-                st.success("Order(s) Updated!" ,icon="👍")
-            else:
-                 st.error("Hash values mismatch. Data integrety may have been compromised.")
-        except Exception as e:
-            st.write(f'Something went wrong: {e}')
-    else:
-         st.success('There are no pending orders right now',icon="👍")	
